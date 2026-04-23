6bdfc5da7adf3772463b47b0647408acafb4fdd6
ramhuw <johndoubleb@gmail.com>
2026-04-23 07:50:32 +0000

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::io::Write;
+use std::io::{Read, Write};
 #[allow(unused_imports)]
 use std::net::TcpListener;
 
@@ -13,7 +13,41 @@ fn main() {
     for stream in listener.incoming() {
         match stream {
             Ok(mut stream) => {
-                stream.write_all(b"HTTP/1.1 200 OK\r\n\r\n").unwrap();
+                let mut buf = [0u8; 1024];
+                let n = stream.read(&mut buf).unwrap();
+                let request = String::from_utf8_lossy(&buf[..n]);
+                let mut request_iter = request.split("\r\n\r\n");
+                let req_header = request_iter.next().unwrap();
+                let mut req_header_iter = req_header.split("\r\n");
+                let req_line = req_header_iter.next().unwrap();
+                let mut req_iter = req_line.split_whitespace();
+                let mut target: Option<&str> = None;
+                let mut version: Option<&str> = None;
+                match req_iter.next() {
+                    Some("GET") => {
+                        target = req_iter.next();
+                        version = req_iter.next();
+                    }
+                    _ => {}
+                }
+                let mut host: Option<&str> = None;
+                let headers = req_header_iter.next().unwrap();
+                let mut header_iter = headers.split("\r\n");
+                while let Some(header) = header_iter.next() {
+                    let mut line_iter = header.split_whitespace();
+                    let method = line_iter.next().unwrap();
+                    match method {
+                        "Host" => {
+                            host = line_iter.next();
+                        }
+                        _ => {}
+                    }
+                }
+                if target == Some("/") {
+                    stream.write_all(b"HTTP/1.1 200 OK\r\n\r\n").unwrap();
+                } else {
+                    stream.write_all(b"HTTP/1.1 404 Not Found\r\n\r\n").unwrap();
+                }
                 println!("accepted new connection");
             }
             Err(e) => {
